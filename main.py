54bfc813c2b2bb128bc2d91a36bc403b0ce06960
Priscilla Abhulimen <priscillaabhulimen@gmail.com>
2026-03-06 00:47:54 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/items")
+async def create_item(item: Item):
+    return {"item": item}
