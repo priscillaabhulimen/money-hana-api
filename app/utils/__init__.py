7c173c5e55805b62f96e1a8f62f3e7e93f6a63a9
Priscilla Abhulimen <priscillaabhulimen@gmail.com>
2026-03-08 23:11:01 -0300

    updated email templet

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,3 +2,4 @@ from app.utils.hash import hash_password, verify, create_access_token, decode_ac
 from app.utils.constants import ERROR_MESSAGES, TEMP_USER_ID
 from app.utils.openapi import custom_openapi
 from app.utils.email import send_verification_email
+from app.utils.email_templates import verification_email
